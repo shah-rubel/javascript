b3c09f6ec54d323d63aba6baf2802bdb0de690f0
saifur-rahman <rafef.anam@gmail.com>
2023-04-06 12:03:37 +0600

    function with return

--- function.js
+++ function.js
@@ -10,3 +10,11 @@ function sum(num1,num2){
     console.log(num1+num2);
 }
 sum(12,23);
+//function that return any value
+
+function squaring(value){
+    const result = value*value;
+    return result;
+}
+const square1 = squaring(5);
+console.log(square1);
