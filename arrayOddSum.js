c1b9081230fd775841726f18ae4f013a6ec71ec2
saifur-rahman <rafef.anam@gmail.com>
2023-04-09 03:16:06 +0600

    sum of odd array elements

--- arrayOddSum.js
+++ arrayOddSum.js
@@ -8,8 +8,25 @@ function add(numberArray){
     return sum;
 }
 
+function oddSum(numberArray){
+    let oddArray=[];
+    for(let i =0;i<numberArray.length;i++){
+        const index = i;
+        const element = numberArray[i];
+        if(element % 2 !== 0){
+            oddArray.push(element);
+        }
+    }
+    const totalOddSum=add(oddArray);
+    return totalOddSum;
+    
+    
+
+}
+
 let array1 = [1,2,3,4,5];
 
-let sumArray1 = add(array1);
+let oddArray1 = oddSum(array1);
 
-console.log('sum',sumArray1);
+
+console.log('sum',oddArray1);
